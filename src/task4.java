5381e2d9466fdf1b82ab4447dd0e14fed6d2b76d
hvngover <hvngover17@gmail.com>
2023-04-09 18:16:26 +0600

    task4.java

---
+++ src/task4.java
@@ -0,0 +1,19 @@
+import java.util.Scanner; // import the Scanner class from the java.util package to read input
+
+public class task4 { // declare a class named Main
+
+    public static void main(String[] args) { // declare a main method, which is the entry point of the program
+        Scanner scanner = new Scanner(System.in); // create a new Scanner object to read input
+        int n = scanner.nextInt(); // read an integer input entered by the user and assign it to the variable n
+        int factorial = factorial(n); // call the factorial method with the argument n and assign the result to the variable factorial
+        System.out.println(factorial); // print the final result of the factorial function to standard output
+    }
+
+    public static int factorial(int n) { // declare a static method named factorial, which computes the factorial of the input argument n recursively
+        if (n == 0) { // base case: the factorial of 0 is 1
+            return 1;
+        } else { // recursive case: n! = n * (n - 1)!
+            return n * factorial(n - 1);
+        }
+    }
+}
