8faf5c51da1c11afeaa3a0135385121f2a9908b9
hvngover <109821023+hvngover@users.noreply.github.com>
2023-04-09 20:10:17 +0600

    Update task5.java

--- src/task5.java
+++ src/task5.java
@@ -1,18 +1,21 @@
-import java.util.Scanner;
+import java.util.Scanner; // Import the Scanner class
 
 public class DigitCheck {
     public static boolean check(String s) {
-        return s.matches("\\d+");
+        // Check if a string consists of only digits
+        return s.matches("\\d+"); // The regular expression \d+ matches one or more digits
     }
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String s = scanner.nextLine();
+        Scanner scanner = new Scanner(System.in); // Create a new Scanner object to read input from the user
+        String s = scanner.nextLine(); // Read the input string from the user
 
-        if (check(s)) {
-            System.out.println("Yes");
+        if (check(s)) { // Check if the input string consists of only digits
+            System.out.println("Yes"); // Print "Yes" if it consists of only digits
         } else {
-            System.out.println("No");
+            System.out.println("No"); // Print "No" if it does not consist of only digits
         }
+
+        scanner.close(); // Close the Scanner object to release any system resources associated with it
     }
 }
